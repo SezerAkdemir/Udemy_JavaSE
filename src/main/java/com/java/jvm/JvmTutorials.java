d6b24fa014b6fb78170a96e769f49e50704cd3bb
SezerAkdemir <sezerakdemir92@gmail.com>
2020-12-07 23:53:11 +0300

    Eclipse Commit UI

--- src/main/java/com/java/jvm/JvmTutorials.java
+++ src/main/java/com/java/jvm/JvmTutorials.java
@@ -2,6 +2,6 @@ package com.java.jvm;
 
 public class JvmTutorials {
 	public static void main(String[] args) {
-		System.out.println("Selam Sezer, Nasılsın?");
+		System.out.println("Selam Sezer, Nasılsın???");
 	}
 }
